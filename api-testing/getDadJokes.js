83f5a6f26ee0ac995cfa1dd00b715feccfc59b78
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-06-20 19:45:02 +0100

    async/await version

--- api-testing/getDadJokes.js
+++ api-testing/getDadJokes.js
@@ -1,10 +1,18 @@
-const getDadJokes = () => {
+const getDadJokes = async () => {
+  const response = await fetch('https://icanhazdadjoke.com', {
+    headers: {
+      'Accept': 'application/json'
+    }
+  })
+  const json = await response.json()
+  console.log(json)
+  /* Original Promise version
   return fetch('https://icanhazdadjoke.com', {
     headers: {
       'Accept': 'application/json'
     }
   }).then(response => response.json()).then(json => console.log(json))
-
+  */
 }
 
 module.exports = getDadJokes
