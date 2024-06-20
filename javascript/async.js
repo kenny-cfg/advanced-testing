f5ad0cae18f653f5fae5fe27881171b7b4387275
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-06-20 18:58:30 +0100

    console.log all over the place

--- javascript/async.js
+++ javascript/async.js
@@ -1,6 +1,8 @@
 const waitSeconds = (numberOfSeconds) => {
+  console.log('SECOND')
   return new Promise((resolve, reject) => {
     setTimeout(() => {
+      console.log('THIRD, after a delay')
       resolve('Time waited')
     }, numberOfSeconds * 1000)
   })
--- javascript/async.test.js
+++ javascript/async.test.js
@@ -2,7 +2,9 @@ const waitSeconds = require('./async');
 
 describe('waitSeconds', () => {
   it('Promise version', () => {
+    console.log('FIRST')
     return waitSeconds(2).then(data => {
+      console.log('FOURTH')
       expect(data).toBe('Time waited')
     })
   })
