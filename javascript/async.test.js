ed88da1e325a125504231a6f8fa77e0f73bdf4ea
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-06-20 19:10:37 +0100

    async/await version

--- javascript/async.test.js
+++ javascript/async.test.js
@@ -8,4 +8,10 @@ describe('waitSeconds', () => {
       expect(data).toBe('Time waited')
     })
   })
+  
+  it('async/await version', async () => {
+    console.log('FIRST')
+    const data = await waitSeconds(2)
+    expect(data).toBe('Time waited')
+  })
 })
