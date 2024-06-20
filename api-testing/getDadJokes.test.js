8052fba43648cde0c721ae86424a36e1042dcaad
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-06-20 20:12:36 +0100

    nicer async/await version

--- api-testing/getDadJokes.test.js
+++ api-testing/getDadJokes.test.js
@@ -2,14 +2,24 @@ const getDadJokes = require("./getDadJokes");
 
 describe('getDadJokes', () => {
   it('works', async () => {
-    global.fetch = jest.fn(() =>
+    global.fetch = jest.fn(async () => {
+      return {
+        json: async () => {
+          return {
+            joke: 'Kenny'
+          }
+        }
+      }
+    }
+      /* Original, ugh
       Promise.resolve({
         json: () => Promise.resolve({ joke: 'Kenny' }),
       })
+      */
     );
-    
+
     const result = await getDadJokes()
-    
+
     expect(result).toBe('Kenny')
   })
 })
