ede7cc902a93149ae6b6406ea42de238a553e44e
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-06-20 20:19:25 +0100

    Some async/await and Promise examples

--- api-testing/getDadJokes.test.js
+++ api-testing/getDadJokes.test.js
@@ -17,6 +17,36 @@ describe('getDadJokes', () => {
       })
       */
     );
+    
+    /*
+    Promise.resolve(something)
+    is equivalent to
+    new Promise((resolve, reject) => resolve(something))
+    */
+    
+    /*
+     Use of reject example
+     const login = (username, password) => {
+       return new Promise((resolve, reject) => {
+         if (username === 'kenny' && password === 'password') {
+           resolve('successful')
+         } else {
+           reject('failed')
+        }
+        })
+      }
+      
+      login(username, password).then(() => console.log('You are logged in')).catch(() => console.log('You are not logged in'))
+      
+      async/await version:
+
+      try {
+        await login(username, password)
+        console.log('You are logged in')
+      } catch (err) {
+        console.log('You are not logged in')
+      }
+    */
 
     const result = await getDadJokes()
 
